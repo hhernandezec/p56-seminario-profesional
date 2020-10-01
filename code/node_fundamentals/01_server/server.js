a970c07a55b5086f23b3bee929092e39c63db438
Xavier hernández <ing.hugoherandez@hotmail.com>
2020-10-01 10:28:48 -0500

    Cambios en el esqueleto de la app

--- code/node_fundamentals/01_server/server.js
+++ code/node_fundamentals/01_server/server.js
@@ -1,25 +1,27 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const router = express.Router()
-const port = 5000
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = express.Router();
+const port = 5000;
+const response = require('./network/response.js');
 
 var app = express()
 app.use(router)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-router.get('carrera', function(req, res) {
-    res.send('Lista de carreras de la UPS')
-})
+router.get('/carrera', function(req, res) {
+    response.success(req, res, 'Lista de carreras de la UPS ', 200);
+});
 
 router.post('/carrera', function(req, res) {
-    console.log(req.body)
-    console.log(req.query)
-
-    res.status(201).send({ tipo_error: 0, mensaje_error: '', mensaje_exito: 'Añadido exitosamente' })
-})
+    if (req.query.error == 'ok') {
+        response.error(req, res, 'Error al ingresar la carrera', 500);
+    } else {
+        response.success(req, res, 'Ingreso de carrera exitosa ', 201);
+    }
+});
 
 app.use('/', express.static('public'))
 
-app.listen(port)
-console.log('la app está escuchando en http://localhost:' + port)
+app.listen(port);
+console.log('la app está escuchando en http://localhost:' + port);
